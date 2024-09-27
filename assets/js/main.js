49d752a184cb36a8aa5535740f7c9820d1d24c56
JazzY6113 <kbogomolov22@gmail.com>
2024-09-27 15:38:36 +0700

    switch on game screen

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+let name = '';
+let game = {};
+let panel = 'start';
+let $ = function (domElement) { return document.querySelector(domElement); };
+
+let nav = () => {
+    document.onclick = (event) => {
+        event.preventDefault();
+        switch (event.target.id) {
+            case "startGame":
+                go('game', 'd-block');
+                break;
+            case "restart":
+                go('game', 'd-block');
+                //Тут будем убирать элементы
+                break;
+        }
+    }
+}
+
+let go = (page, attribute) => {
+    let pages = ['start', 'game', 'end'];
+    panel = page;
+    $(`#${page}`).setAttribute('class', attribute);
+    pages.forEach(el => {
+        if(page !== el) (`#${el}`).setAttribute('class', 'd-none');
+    })
+}
+window.onload = () => {
+    nav();
+    setInterval(() => {
+        if(panel === "game") {
+            game = new Game();
+            game.start();
+            panel = 'game process';
+        }
+    }, 500)
+}
